d76ac615e9135a01ba323565cd9b1695b0fcc426
Fabrice Benhamouda <fabrice.benhamouda@normalesup.org>
2017-04-06 22:56:46 -0400

    add biber tests

--- Makefile
+++ Makefile
@@ -1,16 +1,24 @@
 PYTHON=python2
+BIBER=biber
 BIBTEX=./bibtex8/bibtex
 BIBTEX_FLAGS=--csfile bibtex8/csf/cp850lat.csf --wolfgang
 PDFLATEX=./pplatex/bin/ppdflatex -q --
 
-TEX_TESTS=test_abbrev0.tex test_abbrev0_crossref.tex test_abbrev1.tex test_abbrev1_crossref.tex test_abbrev2.tex test_abbrev2_crossref.tex test_abbrev3.tex  test_abbrev3_crossref.tex
+TEX_TESTS=test_abbrev0_bibtex.tex test_abbrev0_biber.tex test_abbrev0_crossref_bibtex.tex test_abbrev0_crossref_biber.tex test_abbrev1_bibtex.tex test_abbrev1_biber.tex test_abbrev1_crossref_bibtex.tex test_abbrev1_crossref_biber.tex test_abbrev2_bibtex.tex test_abbrev2_biber.tex test_abbrev2_crossref_bibtex.tex test_abbrev2_crossref_biber.tex test_abbrev3_bibtex.tex test_abbrev3_biber.tex test_abbrev3_crossref_bibtex.tex test_abbrev3_crossref_biber.tex
 PDF_TESTS=$(TEX_TESTS:.tex=.pdf)
 
 .PHONY: all clean mrproper FORCE
 
 all: $(TEX_TESTS) $(PDF_TESTS)
 
-%.pdf: %.tex clean FORCE
+%_biber.pdf: %_biber.tex clean FORCE
+	@echo
+	@echo "--- Test of $<"
+	rm -f $(patsubst %.tex,%.aux,$<)
+	$(PDFLATEX) $<
+	$(BIBER) $(basename $<) | sed -e '/WARN - month field/d' | sed -e '/cannot be null, deleting it/d' # remove the issues with the months and the empty fields
+
+%_bibtex.pdf: %_bibtex.tex clean FORCE
 	@echo
 	@echo "--- Test of $<"
 	$(PDFLATEX) $<
--- gen_tests.py
+++ gen_tests.py
@@ -2,7 +2,7 @@
 
 from string import Template
 
-test_template = r"""\documentclass[a4paper,10pt]{article}
+test_template_bibtex = r"""\documentclass[a4paper,10pt]{article}
 \usepackage{url}
 \usepackage{amscd,amsmath}
 \usepackage{amsfonts}
@@ -17,11 +17,36 @@ test_template = r"""\documentclass[a4paper,10pt]{article}
 \end{document}
 """
 
+test_template_biber = r"""\documentclass[a4paper,10pt]{article}
+\usepackage[utf8]{inputenc}
+\usepackage[T1]{fontenc}
+\usepackage{url}
+\usepackage{amscd,amsmath}
+\usepackage{amsfonts}
+\usepackage[cm]{fullpage}
+
+\usepackage{biblatex}
+
+\bibliography{../db/abbrev${short},../db/crypto${crossref}}
+
+\begin{document}
+
+\nocite{*}
+\printbibliography{}
+
+\end{document}
+"""
+
 def main():
     for crossref in ["","_crossref"]:
         for short in range(4):
-            out = file("test_abbrev{}{}.tex".format(short, crossref), "w")
-            out.write(Template(test_template).substitute(short=short, crossref=crossref))
+            out = file("test_abbrev{}{}_bibtex.tex".format(short, crossref), "w")
+            out.write(Template(test_template_bibtex).substitute(short=short, crossref=crossref))
+            out.close()
+    for crossref in ["","_crossref"]:
+        for short in range(4):
+            out = file("test_abbrev{}{}_biber.tex".format(short, crossref), "w")
+            out.write(Template(test_template_biber).substitute(short=short, crossref=crossref))
             out.close()
 
 if __name__ == "__main__":
